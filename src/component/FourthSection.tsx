55adce0fc36ebd79d719fdaab9e10d160bf6601a
Divinegirl8 <iamoluchimercy@gmail.com>
2026-07-10 17:11:03 +0100

    new

--- src/component/FourthSection.tsx
+++ src/component/FourthSection.tsx
@@ -1,3 +1,6 @@
+import card1 from "../assets/FourthSection/1.svg"
+import robot from "../assets/FourthSection/Mask group (5).svg"
+
 const FourthSection = () => {
     return(
         <div
@@ -9,15 +12,39 @@ const FourthSection = () => {
                     <p>How it works</p>
                 </div>
 
-                <h1 className="md:text-center font-normal text-[#171F2A] md:text-[45px] text-[30px] md:max-w-130 font-cal-sans">
+                <h1 className="md:text-center font-normal text-[#171F2A] md:text-[45px] text-[30px] md:max-w-130 font-cal-sans md:mt-4 md:leading-13">
                   Answer. Understand. Act. 
-                 
-                   
-                    In that order, every time.
+                 In that order, every time.
                 </h1> 
-                
-                
+
+
+                <div className="grid md:grid-cols-3 md:px-10 max-sm:gap-10 md:justify-evenly md:items-center mt-10 md:mx-auto">
+                        <div className="hidden md:block">
+                            <img src={card1} />
+                        </div>
+
+                        <div className="md:w-100 md:h-55 border border-[#9C73F3] rounded-[15px] px-8 py-4 font-manrope md:hidden block">
+
+                            <img src={robot}/>
+                            <h5 className="text-[#171F2A] text-[20px] font-semibold ">Understand</h5>
+                            <p className="text-[#171F2A] text-[14px] mt-1">Sernio listens for what the caller actually needs: booking, billing, a refill, or something urgent, using your practice's own knowledge base, not a generic script.</p>
+                            </div>
+
+
+                        <div className="md:w-100 md:h-55 border border-[#9C73F3] rounded-[15px] px-8 py-4 font-manrope">
+
+                            <img src={robot}/>
+                            <h5 className="text-[#171F2A] text-[20px] font-semibold ">Understand</h5>
+                            <p className="text-[#171F2A] text-[14px] mt-1">Sernio listens for what the caller actually needs: booking, billing, a refill, or something urgent, using your practice's own knowledge base, not a generic script.</p>
+                        </div>
+
+                        <div className="md:w-100 md:h-55 border border-[#9C73F3] rounded-[15px] px-8 py-4">
+                        <img src={robot}/>
+                        <h5 className="text-[#171F2A] text-[20px] font-semibold ">Act</h5>
+                        <p className="text-[#171F2A] text-[14px] mt-1">It books the visit, updates the patient record, verifies insurance, or routes urgent calls straight to your on-call line, no transcript left for a human to re-type later.</p>
+                        </div>
                 </div>
+            </div>
 
         </div>
     )
